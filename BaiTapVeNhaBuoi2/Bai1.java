104e8e1e3a7862ea74c7825a41fcd30f0372dffb
Cuong Nguyen <cuongasd52@gmail.com>
2025-02-19 20:35:40 +0700

    Bai 1

---
+++ BaiTapVeNhaBuoi2/Bai1.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Bai1 {
+    static public void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double mark;
+        while(true){
+            try{
+                mark  = scanner.nextDouble();
+                if (mark<0 || mark >10){
+                    throw new IllegalArgumentException("Diem so phai nam trong khoang 1 toi 10");
+                }
+                break;
+
+            }catch(IllegalArgumentException e){
+                System.out.println("Loi: "+ e.getMessage());
+            }
+
+        }
+        if (mark <5) System.out.println("Yeu");
+        else if (5<= mark && mark<6.5) System.out.println("Trung binh");
+        else if (6.5<= mark && mark<7) System.out.println("Trung binh kha");
+        else if (7<= mark && mark<8) System.out.println("Kha");
+        else if (8<= mark && mark<9) System.out.println("Gioi");
+        else System.out.println("Xuat sac");
+        scanner.close();
+    }
+}
